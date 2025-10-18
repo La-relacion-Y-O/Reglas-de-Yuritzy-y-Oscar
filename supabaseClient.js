e8fa77e2564c35b9b7c406182f143ffc39f1f3f6
Novios <faltoguiss@gmail.com>
2025-10-18 15:11:59 -0500

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Estos valores serán reemplazados automáticamente por GitHub Actions durante el despliegue.
-const supabaseUrl = '__SUPABASE_URL__';
-const supabaseAnonKey = '__SUPABASE_ANON_KEY__';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
